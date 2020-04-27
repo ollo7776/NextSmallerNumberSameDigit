e54614735ff18bfaa4bfbfe55f31591d0a2f335e
ollo7 <olka11>
2020-04-27 23:24:45 +0200

    Be or not to be!

--- src/NextSmaller.java
+++ src/NextSmaller.java
@@ -1,4 +1,6 @@
-
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class NextSmaller {
     public static long nextSmaller(long n) {
@@ -20,20 +22,34 @@ public class NextSmaller {
             }
         }
         String nrSide = "";
-        System.out.println("ia: " + ia + " ib: " + ib + " a: " + a + " b: " + b);
+        //System.out.println("ia: " + ia + " ib: " + ib + " a: " + a + " b: " + b);
         String lSide = sn.substring(0, ib);
         String rSide = sn.substring(ia + 1, sn.length());
         rSide += "" + b;
-        char[] crSide = rSide.toCharArray();
-        for (int j = crSide.length - 1; j >= 0; j--) {
-            nrSide += crSide[j];
+        //System.out.println(rSide);
+        int[] rSideArray = new int[rSide.length()];
+        for (int j = 0; j < rSideArray.length; j++) {
+            rSideArray[j] = Character.getNumericValue(rSide.charAt(j));
         }
-        System.out.println("lSide: " + lSide + " nrSide: " + nrSide);
-        StringBuilder sb = new StringBuilder();
-
-        System.out.println(sb);
-        result = lSide + "" + a + "" + rSide;
-        System.out.println("result " + result);
-        return n;
+        Arrays.sort(rSideArray);
+        reverse(rSideArray);
+        rSide = "";
+        for (int item : rSideArray) {
+            rSide += item;
+        }
+        //System.out.println("rSide " + rSide);
+        result = ""+lSide+""+a+""+rSide;
+        return Integer.parseInt(result);
     }
+
+    private static void reverse(int[] input) {
+        int last = input.length - 1;
+        int middle = input.length / 2;
+        for (int j = 0; j <= middle; j++) {
+            int temp = input[j];
+            input[j] = input[last - j];
+            input[last - j] = temp;
+        }
+    }
+
 }
